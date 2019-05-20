910f4b5c4a0ba988e435173b7bc99de57eac2ad2
Isa-Oliveira <50581090+Isa-Oliveira@users.noreply.github.com>
2019-05-20 10:16:16 -0300

    Arquivo

---
+++ jogoforca.py
@@ -0,0 +1,58 @@
+url= 'https://www.ime.usp.br/~pf/dicios/br-sem-acentos.txt'
+import requests
+p = requests.get(url)
+palavras = p.text.lower()
+palavras = palavras.split()
+len(palavras)
+from random import choice
+choice(palavras)
+sorteada = choice(palavras)
+
+def escolhe():
+    from random import choice
+    return choice(palavras)
+
+def desenha(erradas):
+    return print(forca[len(erradas)])
+
+def jogar_novamente():
+    r=input('Deseja jogar novamente?')
+    return r in 'Sim'
+    
+
+def ganhou(sorteada, certas):
+    return set(sorteada) == set(certas)
+
+def chute(letras):
+    from string import ascii_letters ##punctuation
+    while True:
+        x=input('Digite letra:')[0]
+        if x in letras:
+            print('Letra repitida')
+        elif x not in ascii_letters:
+            print('Caracter inválido')
+        elif len(x)!=1:
+            print('Só uma letra1')
+        else:
+            return x
+
+forca = [''' ''',''' o ''',''' o_ ''',''' _o_ ''', '''_o_/''', '''\_o_/''']
+certas=erradas=''
+
+ganhou(sorteada,certas)
+
+while not ganhou(sorteada,certas)':
+    print(sorteada)
+    letra = chute(certas+erradas)
+    if letra in sorteada:
+        certas +=letra
+    else:
+        erradas +=letra    
+    for c in sorteada:
+        if c in certas:
+            print(c, end = ' ')
+        else:
+            print('_', end = ' ')
+    desenha(erradas)        
+    
+#if jogar_novamente(r): 

6094a41548a77940e5a5ac906ff937ab7496e3c2
Gabriel Borges <gabrielfr.borges@gmail.com>
2019-05-20 22:56:45 -0300

    Condição p/ derrota & print fim de jogo e forca

--- jogoforca.py
+++ jogoforca.py
@@ -1,32 +1,93 @@
 url = 'https://www.ime.usp.br/~pf/dicios/br-sem-acentos.txt'
 import requests
 from random import choice
-from string import ascii_letters ##punctuation
+from string import ascii_letters
 p = requests.get(url)
 palavras = p.text.lower()
 palavras = palavras.split()
 sorteada = choice(palavras)
 r = True
-forca = [''' ''', ''' o ''', ''' o_ ''', ''' _o_ ''', '''_o_/''', '''\_o_/''']
+forca = ['''
+    +-----+
+    |     |
+          |
+          |
+          |
+          |
+============''',
+         '''
+    +-----+
+    |     |
+    O     |
+          |
+          |
+          |
+============''',
+         '''
+    +-----+
+    |     |
+    O     |
+    |     |
+          |
+          |
+============''',
+         '''
+    +-----+
+    |     |
+    O     |
+   /|     |
+          |
+          |
+============''',
+         '''
+    +-----+
+    |     |
+    O     |
+   /|\    |
+          |
+          |
+============''',
+         '''
+    +-----+
+    |     |
+    O     |
+   /|\    |
+   /      |
+          |
+============''',
+         '''
+    +-----+
+    |     |
+    O     |
+   /|\    |
+   / \    |
+          |
+============''']
 
 def escolhe():
     return choice(palavras)
 
 def desenha(erradas):
-    return print( forca[len(erradas)] )
+    return print(forca[len(erradas)] )
 
 def jogar_novamente():
     global sorteada
     sorteada = choice(palavras)
-    r = input('Deseja jogar novamente(Ss/nn)? ').lower()
+    r = input('Deseja jogar novamente(S/n)? ').lower()
     return r in 'sim'
 
 def ganhou(sorteada, certas):
     return set(sorteada) == set(certas)
 
+def fim_de_jogo(sorteada, certas):
+    if set(sorteada) == set(certas):
+        return print(f'PARABÉNS! Você acertou a palavra "{sorteada}" =D')
+    else:
+        return print(f'Você não acertou a palavra "{sorteada}" =(')
+
 def chute(letras):
     while True:
-        x = input('Digite letra:')
+        x = input('\nDigite letra:')
         if x in letras:
             print('Letra repitida')
         elif x not in ascii_letters:
@@ -38,20 +99,22 @@ def chute(letras):
 
 while r:
     certas = erradas = ''
-    
+    desenha(erradas)
+
     while not ganhou(sorteada, certas):
-        print(sorteada) #deletar
         letra = chute(certas + erradas)
         if letra in sorteada:
             certas += letra
         else:
-            erradas += letra    
+            erradas += letra
+        desenha(erradas)
         for c in sorteada:
             if c in certas:
                 print(c, end = ' ')
             else:
                 print('_', end = ' ')
-        desenha(erradas)
-    #acrescentar derrota
+        print()
+        if len(erradas) == (len(forca)-1):
+            break
+    fim_de_jogo(sorteada, certas)
     r = jogar_novamente()
-
